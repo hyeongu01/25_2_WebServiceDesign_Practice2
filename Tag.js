511e295e765c5c153a32ab02c35504ca0a5d3c43
hyeongu01 <cdy5212@gmail.com>
2025-11-25 03:06:53 +0900

    feat: add Tag.js

---
+++ Tag.js
@@ -0,0 +1,15 @@
+class Tag {
+    constructor(id, name, createdAt = Date.now()) {
+        this.id = id;
+        this.name = name;
+        this.createdAt = createdAt
+    }
+
+    static fromJson(json) {
+
+    }
+
+    static fromString(str) {
+
+    }
+}
