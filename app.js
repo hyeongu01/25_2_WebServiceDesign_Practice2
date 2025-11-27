dde8ea0a1e0719fd7a3f48270442db8e07be196c
hyeongu01 <cdy5212@gmail.com>
2025-11-28 03:54:44 +0900

    chore: 폴더 구조 재배치

--- app.js
+++
@@ -1,8 +0,0 @@
-const express = require("express");
-const app = express();
-
-const memoRouter = require("./routers/memoRouter");
-
-
-
-app.use("/api/memos", memoRouter);
---
+++ src/app.js
@@ -0,0 +1,26 @@
+const express = require("express");
+const app = express();
+
+const memoRouter = require("./routers/memoRouter");
+
+
+app.use("/api/memos", memoRouter);
+
+
+app.get("/", (req, res) => {
+    res.status(200).json({
+        message: "Memo API Server is running",
+        timestamp: Date.now()
+    });
+});
+
+// 정의되지 않은 경로
+app.use((req, res, next) => {
+    res.status(404).json({
+        error: "not_found",
+        message: "요청한 API endpoint 를 찾을 수 없습니다."
+    });
+});
+
+
+module.exports = app;
